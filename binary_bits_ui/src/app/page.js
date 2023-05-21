325832c5c533f2dc919670257c3ae5bcc7d68c12
surya kant <suryakant0013@yahoo.in>
2023-05-21 21:05:17 +0530

    added sign-in,up, navbar and home page demo

--- binary_bits_ui/src/app/page.js
+++ binary_bits_ui/src/app/page.js
@@ -1,113 +1,176 @@
-import Image from 'next/image'
+'use client'
+
+import React from 'react'
+import { Menu, X } from 'lucide-react'
+
+
+
+const posts = [
+  {
+    category: 'Design',
+    title: '10 Tips for Crafting the Perfect UX Portfolio',
+    description: 'Learn how to showcase your design skills and stand out in a crowded job market.',
+    author: 'Emily Lee',
+    date: '3 April 2023',
+    avatar: 'https://www.uifaces.co/wp-content/uploads/2022/01/uifaces-logo.svg',
+    poster:
+      'https://images.unsplash.com/photo-1573164713714-d95e436ab8d6?ixlib=rb-4.0.3&auto=format&fit=crop&w=1469&q=80',
+  },
+  {
+    category: 'Technology',
+    title: 'The Future of Mobile App Development',
+    description:
+      'Discover the latest trends and techniques that will shape the future of mobile app development.',
+    author: 'John Smith',
+    date: '1 April 2023',
+    avatar: 'https://randomuser.me/api/portraits/men/32.jpg',
+    poster:
+      'https://images.unsplash.com/photo-1618761714954-0b8cd0026356?ixlib=rb-4.0.3&auto=format&fit=crop&w=1170&q=80',
+  },
+  {
+    category: 'Business',
+    title: 'How to Launch a Successful Startup',
+    description:
+      'Learn the essential steps to launch a successful startup and make your dreams a reality.',
+    author: 'Sarah Brown',
+    date: '28 March 2023',
+    avatar: 'https://randomuser.me/api/portraits/women/44.jpg',
+    poster:
+      'https://images.unsplash.com/photo-1559136555-9303baea8ebd?ixlib=rb-4.0.3&auto=format&fit=crop&w=1470&q=80',
+  },
+  {
+    category: 'Health',
+    title: 'The Benefits of Mindfulness Meditation',
+    description:
+      'Discover the scientifically proven benefits of mindfulness meditation and how it can improve your health and wellbeing.',
+    author: 'David Kim',
+    date: '25 March 2023',
+    avatar: 'https://randomuser.me/api/portraits/men/46.jpg',
+    poster:
+      'https://images.unsplash.com/photo-1591228127791-8e2eaef098d3?ixlib=rb-4.0.3&auto=format&fit=crop&w=1470&q=80',
+  },
+  {
+    category: 'Education',
+    title: 'Why Learning a Second Language is Important',
+    description:
+      'Explore the benefits of learning a second language and how it can improve your cognitive abilities.',
+    author: 'Maria Rodriguez',
+    date: '22 March 2023',
+    avatar: 'https://randomuser.me/api/portraits/men/97.jpg',
+    poster:
+      'https://images.unsplash.com/photo-1634128221889-82ed6efebfc3?ixlib=rb-4.0.3&auto=format&fit=crop&w=1470&q=80',
+  },
+  {
+    category: 'Travel',
+    title: 'The Best Places to Visit in Europe',
+    description: 'Discover the top destinations in Europe and plan your dream vacation.',
+    author: 'Alex Johnson',
+    date: '19 March 2023',
+    avatar: 'https://randomuser.me/api/portraits/men/99.jpg',
+    poster:
+      'https://images.unsplash.com/photo-1663616132598-e9a1ee3ad186?ixlib=rb-4.0.3&auto=format&fit=crop&w=1470&q=80',
+  },
+  {
+    category: 'Food',
+    title: 'How to Make the Perfect Cup of Coffee',
+    description:
+      'Learn the secrets to making the perfect cup of coffee and impress your friends and family.',
+    author: 'Thomas Lee',
+    date: '16 March 2023',
+    avatar: 'https://randomuser.me/api/portraits/women/63.jpg',
+    poster:
+      'https://images.unsplash.com/photo-1426260193283-c4daed7c2024?ixlib=rb-4.0.3&auto=format&fit=crop&w=1476&q=80',
+  },
+  {
+    category: 'Fashion',
+    title: 'The Latest Fashion Trends for Spring 2023',
+    description:
+      'Discover the hottest fashion trends for the upcoming spring season and stay ahead of the curve.',
+    author: 'Jessica Kim',
+    date: '13 March 2023',
+    avatar: 'https://randomuser.me/api/portraits/women/47.jpg',
+    poster:
+      'https://images.unsplash.com/photo-1470240731273-7821a6eeb6bd?ixlib=rb-4.0.3&auto=format&fit=crop&w=1470&q=80',
+  },
+  {
+    category: 'Sports',
+    title: 'The Benefits of Yoga for Athletes',
+    description:
+      'Learn how practicing yoga can improve your athletic performance and prevent injuries.',
+    author: 'Michael Johnson',
+    date: '10 March 2023',
+    avatar: 'https://randomuser.me/api/portraits/men/86.jpg',
+    poster:
+      'https://plus.unsplash.com/premium_photo-1663012880499-47f1ca50459d?ixlib=rb-4.0.3&auto=format&fit=crop&w=1470&q=80',
+  },
+]
+
 
 export default function Home() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false)
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center justify-between px-10">
+      {/* posts */}
+      <div className="grid gap-6 gap-y-10 py-6 md:grid-cols-2 lg:grid-cols-3">
+        {posts.map((post) => (
+          <div key={post.title} className="border">
+            <img src={post.poster} className="aspect-video w-full rounded-md" alt="" />
+            <div className="min-h-min p-3">
+              <p className="mt-4 w-full text-xs font-semibold leading-tight text-gray-700">
+                #{post.category}
+              </p>
+              <p className="mt-4 flex-1 text-base font-semibold text-gray-900">{post.title}</p>
+              <p className="mt-4 w-full text-sm leading-normal text-gray-600">
+                {post.description}
+              </p>
+              <div className="mt-4 flex space-x-3 ">
+                <img className="h-full w-10 rounded-lg" src={post.avatar} alt={post.author} />
+                <div>
+                  <p className="text-sm font-semibold leading-tight text-gray-900">
+                    {post.author}
+                  </p>
+                  <p className="text-sm leading-tight text-gray-600">{post.date}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="w-full border-t border-gray-300">
+        <div className="mt-2 flex items-center justify-between">
+          <div className="hidden md:block">
+            <p>
+              showing <strong>1</strong> to <strong>10</strong> of <strong>20</strong> results
+            </p>
+          </div>
+          <div className="space-x-2">
+            <button
+              type="button"
+              className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+            >
+              &larr; Previous
+            </button>
+            <button
+              type="button"
+              className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+            >
+              Next &rarr;
+            </button>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    
+    </main >
   )
 }
+
+
+
+
+
+
