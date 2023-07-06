d53bf2bd1c6055ded22fcc3fe723a1bfc43ba7ba
surya kant <suryakant0013@yahoo.in>
2023-07-06 16:30:55 +0530

    added userLogin in controller

---
+++ userAuthenticationService/src/users/controllers/userLogin.js
@@ -0,0 +1,26 @@
+const userService = require("../services/userService");
+const { isEmailValid, isPassValid } = require("../../helpers");
+
+const userLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ error: "email and password are required" });
+    }
+    if (!isEmailValid(email)) {
+      return res.status(400).json({ error: "Invalid email" });
+    }
+    if (!isPassValid(password)) {
+      return res
+        .status(400)
+        .json({
+          error:
+            "password should contain atleast one special char,one number,one uppercase letter,one lowercase letter",
+        });
+    }
+    await userService.userLogin(req, res);
+  } catch (error) {
+    console.log("error", error);
+    return res.status(500).json({ error: error });
+  }
+};
