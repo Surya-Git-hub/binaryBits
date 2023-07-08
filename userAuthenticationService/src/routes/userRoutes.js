fbbe71dd1e25ddcc6e721e09b9327ed3e0293a8c
surya kant <suryakant0013@yahoo.in>
2023-07-08 20:10:51 +0530

    added routes for profile

--- userAuthenticationService/src/routes/userRoutes.js
+++ userAuthenticationService/src/routes/userRoutes.js
@@ -17,13 +17,16 @@ router.post("/magic-login", magicController.login);
 router.get("/verify-magic-link", magicController.verifyLink);
 
 //profile routes
-router.post("/create-profile",isAuthenticated,isEmailVerified,profileController.create);
+router.get("/:userId", profileController.get);
+router.post("/create-profile", isAuthenticated, isEmailVerified, profileController.create);
+router.patch("/profile", isAuthenticated, isEmailVerified, profileController.update);
+router.delete("/profile", isAuthenticated, isEmailVerified, profileController.remove)
 
 //get user routes
 router.get("/some", userController.getSome);
 router.get("/:userId", userController.getOne);
 router.get("/", userController.getAll);
-router.get("/protected",isAuthenticated,isEmailVerified,userController.getAll);
+router.get("/protected", isAuthenticated, isEmailVerified, userController.getAll);
 
 //verification routes
 router.get("/verify", emailController.verify);
