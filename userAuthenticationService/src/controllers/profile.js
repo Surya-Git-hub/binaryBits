dda93313f691abc39bc4ca0f2949ee6604ed401e
surya kant <suryakant0013@yahoo.in>
2023-07-08 20:03:21 +0530

    added crud to profile controller and services

--- userAuthenticationService/src/controllers/profile.js
+++ userAuthenticationService/src/controllers/profile.js
@@ -8,8 +8,8 @@ const {
 
 const create = async (req, res) => {
     try {
-        const { profession, bio, imageFile } = req.body;
-        if (!hasValue(profession) && !hasValue(bio) && !hasValue(imageFile)) {
+        const { profession, bio, profilePhoto, dob, location, github, socials, organizations } = req.body;
+        if (!hasValue(profession) && !hasValue(bio) && !hasValue(profilePhoto) && !hasValue(dob) && !hasValue(location) && !hasValue(github) && !hasValue(socials) && !hasValue(organizations)) {
             return res
                 .status(200)
                 .json({ status: "Nothing saved because nothing to save" });
@@ -22,20 +22,20 @@ const create = async (req, res) => {
         if (!bioRegex.test(bio) && bio !== "") {
             return res.status(400).json({ error: "bio is invalid" });
         }
-        if (imageFile) {
-            const allowedExtensions = [".jpg", ".jpeg", ".png", ".svg", ".img"];
-            const fileExtension = imageFile.originalname
-                .split(".")
-                .pop()
-                .toLowerCase();
-            if (!allowedExtensions.includes(fileExtension)) {
-                return res.status(400).json({ error: "Invalid imageFile extension" });
-            }
-            const maxSize = 10 * 1024 * 1024; // 10MB
-            if (imageFile.size > maxSize) {
-                return res.status(400).json({ error: "File size exceeds the limit" });
-            }
-        }
+        // if (profilePhoto) {
+        //     const allowedExtensions = [".jpg", ".jpeg", ".png", ".svg", ".img"];
+        //     const fileExtension = imageFile.originalname
+        //         .split(".")
+        //         .pop()
+        //         .toLowerCase();
+        //     if (!allowedExtensions.includes(fileExtension)) {
+        //         return res.status(400).json({ error: "Invalid imageFile extension" });
+        //     }
+        //     const maxSize = 10 * 1024 * 1024; // 10MB
+        //     if (imageFile.size > maxSize) {
+        //         return res.status(400).json({ error: "File size exceeds the limit" });
+        //     }
+        // }
 
         await profileService.create(req, res);
     } catch (error) {
@@ -44,6 +44,87 @@ const create = async (req, res) => {
     }
 };
 
+const get = async (req, res) => {
+    try {
+        const id = req.params?.userId;
+        if (!hasValue(id)) {
+            return res.status(400).json({ error: "userId is required in params" });
+        }
+        await profileService.get(req, res);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: error });
+
+    }
+}
+
+const update = async () => {
+    try {
+
+        const { updates } = req.body;
+        if (!hasValue(updates)) {
+            return res
+                .status(200)
+                .json({ status: "Nothing saved because nothing to save" });
+        }
+        // Perform data validation
+        const validUpdates = {};
+
+        // Validate and add each field to the validUpdates object
+        if (typeof updates.bio === 'string') {
+            validUpdates.bio = updates.bio;
+        }
+
+        if (typeof updates.profilePhoto === 'string') {
+            validUpdates.profilePhoto = updates.profilePhoto;
+        }
+
+        if (typeof updates.profession === 'string') {
+            validUpdates.profession = updates.profession;
+        }
+
+        if (updates.dob instanceof Date) {
+            validUpdates.dob = updates.dob;
+        }
+
+        if (typeof updates.location === 'string') {
+            validUpdates.location = updates.location;
+        }
+
+        if (typeof updates.github === 'string') {
+            validUpdates.github = updates.github;
+        }
+
+        if (Array.isArray(updates.socials)) {
+            validUpdates.socials = updates.socials;
+        }
+
+        if (Array.isArray(updates.organizations)) {
+            validUpdates.organizations = updates.organizations;
+        }
+        await profileService.update(req, res);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: error });
+
+    }
+}
+
+const remove = async () => {
+    try {
+        await profileService.remove(req, res);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: error });
+
+    }
+}
+
+
+
 module.exports = {
     create,
+    get,
+    update,
+    remove,
 };
--- userAuthenticationService/src/services/profile.js
+++ userAuthenticationService/src/services/profile.js
@@ -1,39 +1,87 @@
 const { Prisma, PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const { uploadImageToS3 } = require("../utils/awsUploadImage");
-const crypto = require("crypto");
 
 
 const create = async (req, res) => {
-    try {
-      const { bio, profession, id } = req.body;
-      const imageFile = req.files.imageFile.data;
-      console.log("image >>", imageFile);
-      const generateFileName = (bytes = 32) =>
-        crypto.randomBytes(bytes).toString("hex");
-      const s3Response = await uploadImageToS3(imageFile, generateFileName);
-      const profileToInsert = {
-        bio,
-        profilePhoto: generateFileName,
-        profession,
-        user: id,
-      };
-      const createdProfile = await prisma.Profile.create({
-        data: profileToInsert,
-      });
-      const updatedUser = await prisma.User.update({
-        where: { id: id },
-        data: {
-          profile: createdProfile.id,
-        },
-      });
-      res.status(201).json({ savedProfile, updatedUser });
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ message: "internal server error" });
-    }
-  };
+  try {
+    const { profession, bio, profilePhoto, dob, location, github, socials, organizations } = req.body;
+    const profileToInsert = {
+      profession, bio, profilePhoto, dob, location, github, socials, organizations, userId: req.body.id
+    };
+    const createdProfile = await prisma.profile.create({
+      data: profileToInsert,
+    });
 
-  module.exports = {
-    create
+    return res.status(201).json({
+      message: "User profile created successfully",
+      success: true,
+      createdProfile,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "internal server error" });
   }
+};
+
+const get = async (req, res) => {
+  try {
+    const id = req.params?.userId;
+    const profile = await prisma.profile.findUnique({
+      where: {
+        userId: id
+      }
+    })
+    return res.status(200).json({
+      message: "user profile",
+      success: true,
+      profile: profile
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "internal server error" })
+  }
+}
+
+const update = async (req, res) => {
+  try {
+    const existingProfile = await prisma.profile.findUnique({
+      where: { userId: req.body.id },
+    });
+    if (!existingProfile) {
+      return res.status(404).json({ error: 'Profile not found' });
+    }
+    const { updates } = req.body.updates
+    const updatedProfile = await prisma.profile.update({
+      where: { userId: req.body.id },
+      data: updates,
+    });
+    return res.status(200).json({
+      message: "user profile updated",
+      success: true,
+      profile: updatedProfile
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "internal server error" })
+  }
+}
+
+const remove = async (req, res) => {
+  try {
+    const existingProfile = await prisma.profile.delete({
+      where: { userId: req.body.id },
+    });
+    return res.status(200).json({
+      message: "user profile deleted",
+      success: true,
+      count: existingProfile
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "internal server error" })
+  }
+}
+
+module.exports = {
+  create, get, update, remove
+}
