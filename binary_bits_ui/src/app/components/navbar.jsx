760366fe9684b6125aaceb77de8996a2a5a4d7c7
surya kant <suryakant0013@yahoo.in>
2023-07-14 00:01:25 +0530

    made navbar sticky

--- binary_bits_ui/src/app/components/navbar.jsx
+++ binary_bits_ui/src/app/components/navbar.jsx
@@ -30,7 +30,7 @@ export default function Navbar() {
     }
 
     return (
-        <div className="relative w-full bg-white">
+        <div className="relative w-full bg-white sticky top-0">
             <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2 sm:px-6 lg:px-8">
                 <div className="inline-flex items-center space-x-2">
                     <span>
