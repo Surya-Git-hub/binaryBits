87ae2b1dad9be2522130ade5aa6bfe30e4f777c6
surya kant <suryakant0013@yahoo.in>
2023-07-29 23:23:18 +0530

    added a new signup email page

--- binary_bits_ui/src/app/layout.js
+++ binary_bits_ui/src/app/layout.js
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
 
-      <body className={inter.className}>
+      <body className={inter.className + " "+"flex flex-col justify-items-end min-h-screen"} >
         <Context>
         <Navbar />
         {children}
